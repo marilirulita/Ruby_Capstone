d7833579375c90df7dd63eb63a3a8d6656b2650c
Trictonicmp <code_shadow@hotmail.com>
2022-08-02 11:51:05 -0500

    fix linter errors

--- lib/classes/item.rb
+++ lib/classes/item.rb
@@ -1,16 +1,15 @@
 require 'date'
-#https://apidock.com/ruby/Date/strptime/class
-class Item  
+# https://apidock.com/ruby/Date/strptime/class
+class Item
   attr_reader :publish_date, :id, :archived
   attr_accessor :genre, :author, :label, :source
 
-  public
   def initialize(publish_date, id = rand(1..1000), archived: false)
     @id = id
     @publish_date = Date.strptime(publish_date, '%Y-%m-%d')
     @archived = archived
   end
-  
+
   def add_genre(genre)
     genre.add_item(self) unless genre.items.include?(self)
   end
@@ -26,12 +25,13 @@ class Item
   def add_source(source)
     source.add_item(self) unless source.items.include?(self)
   end
-  
+
   def move_to_archive
     @archived = true if can_be_ archived?
   end
-  
+
   private
+
   def can_be_archived?
     @publish_date < Date.new(Date.today.year - 10, Date.today.month, Date.today.day)
   end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,7 +2,7 @@ def list_all(arr, label)
   puts "List of all #{label}"
   print 'title | author | genre | source'
   arr.each_with_index do |item, index|
-    print "#{index + 1}"
+    print (index + 1).to_s
     print "#{item.label.title} | "
     print "#{item.author.first_name} "
     print "#{item.author.last_name} | "
