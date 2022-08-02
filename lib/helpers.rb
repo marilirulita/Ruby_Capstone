9b9ede726e0adfd3c09c330840b6fbe727ff6308
Trictonicmp <code_shadow@hotmail.com>
2022-08-02 17:52:36 -0500

    Add create functions for Genre, Source, Label, Author classes

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,8 @@
+require_relative 'classes/movie'
+require_relative 'classes/genre'
+require_relative 'classes/label'
+require_relative 'classes/author'
+require_relative 'classes/movie'
 require 'date'
 
 def list_all(arr, label)
@@ -21,4 +26,49 @@ def exit
   puts 'Thanks for using this App'
 def generate_id
   rand(1..1000)
+end
+
+def create_movie
+  movie = Movie.new()
+end
+
+def ask_publish_date
+  print 'Publish date [yyyy-mm-dd]: '
+  date = gets.chomp
+end
+
+def create_genre
+  id = generate_id
+  print 'Genre: '
+  name = gets.chomp
+
+  Genre.new(id, name)
+end
+
+def create_source
+  id = generate_id
+  print 'Source: '
+  name = gets.chomp
+
+  Source.new(id, name)
+end
+
+def create_label
+  id = generate_id
+  print 'Title: '
+  title = gets.chomp
+  print 'Color: '
+  color = gets.chomp
+
+  Label.new(id, title, color)
+end
+
+def create_author
+  id = generate_id
+  print 'First name: '
+  name = gets.chomp
+  print 'Last name: '
+  las_name = gets.chomp
+
+  Author.new(id, name, las_name)
 end
