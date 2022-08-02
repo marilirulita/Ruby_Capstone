430065319d0bf724e75103677aaf57de3b19add7
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-02 17:48:40 -0500

    Add unit tests for Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/classes/book'
+
+describe Book do
+  before :each do 
+    @book = Book.new('publisher 1', 'cover_state', '2012-01-16', true)
+  end
+
+  it 'Return a book' do
+    expect(@book).to be_a_instance_of Book
+  end
+  
+  it 'Book has a publisher' do
+    expect(@book.publisher).to eq 'publisher 1'
+  end
+
+  it 'Book should have a cover state' do
+    expect(@book.cover_state).to eq 'cover_state'
+  end
+
+  it 'Book should have a publish date' do
+    expect(@book.publish_date.strftime("%Y-%m-%d")).to eq("2012-01-16")
+  end
+
+  it 'Book should not be archived' do
+    expect(@book.archived).to eq true
+  end
+
+  it 'Book should have an id' do
+    expect(@book.id).to be_a_kind_of Integer
+  end
+end
