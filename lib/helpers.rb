426e8bb8af9691ddf4970e19defaf6c338ad01ad
Trictonicmp <code_shadow@hotmail.com>
2022-08-02 11:28:54 -0500

    Add helpers file to store helpers functions

---
+++ lib/helpers.rb
@@ -0,0 +1,12 @@
+def list_all(arr, label)
+  puts "List of all #{label}"
+  print 'title | author | genre | source'
+  arr.each_with_index do |item, index|
+    print "#{index + 1}"
+    print "#{item.label.title} | "
+    print "#{item.author.first_name} "
+    print "#{item.author.last_name} | "
+    print "#{item.genre.name} | "
+    print "#{item.source.name} \n"
+  end
+end
