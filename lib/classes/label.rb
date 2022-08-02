b5e00125afb65600891424cc2ad88a7b7d76a434
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-02 17:53:49 -0500

    Add Label class

---
+++ lib/classes/label.rb
@@ -0,0 +1,14 @@
+class Label 
+  attr_reader :title, :color, :items
+  def initialize(title, color, id = rand(1..1000), items = [])
+    @title = title
+    @color = color
+    @id = id
+    @items = items
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
+end
