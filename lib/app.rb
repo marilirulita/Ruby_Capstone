671ef8acf7a114d79714794eb729b82383b96a74
Trictonicmp <code_shadow@hotmail.com>
2022-08-02 19:47:46 -0500

    Fix linter error

--- lib/app.rb
+++ lib/app.rb
@@ -70,8 +70,8 @@ class App
       exit
     else 'Choose a valid option'
     end
-    
-    self.run
+
+    run
   end
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/MethodLength
