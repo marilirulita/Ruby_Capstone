2e2aa9c4f48a99a2499be28854fc7cab0a542440
mar <marilirulita@gmail.com>
2022-08-02 23:48:05 -0700

    fix linters errors

--- lib/classes/genre.rb
+++ lib/classes/genre.rb
@@ -2,7 +2,9 @@ class Genre
   attr_accessor :items
   attr_reader :id, :name
 
-  def initialize(id = rand(1..1000), name)
+  def initialize(name, id = rand(1..1000))
+    @name = name
+    @id = id
     @items = []
   end
 
