70a1a4d4bd59f5989956b7b967e3f3767e07f3f0
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-03 15:27:23 -0500

    Add to_json method to Book and Label class

--- lib/classes/book.rb
+++ lib/classes/book.rb
@@ -12,4 +12,14 @@ class Book < Item
   def can_be_archived?
     super || cover_state == 'bad'
   end
+
+  def to_json(*_args)
+    {
+      id: id,
+      publisher: @publisher,
+      cover_state: @cover_state,
+      publish_date: publish_date,
+      archived: archived
+    }.to_json
+  end
 end
