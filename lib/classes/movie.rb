60c74f47c052f8f9ea425fd13738fa0154e1d638
Trictonicmp <code_shadow@hotmail.com>
2022-08-03 15:10:59 -0500

    Add to_json to Movie class

--- lib/classes/movie.rb
+++ lib/classes/movie.rb
@@ -9,4 +9,12 @@ class Movie < Item
   def can_be_archived?
     super || @silet ? true : false
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'silet' => @silet
+    }.to_json(*args)
+  end
 end
