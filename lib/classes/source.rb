71ae175ac73eba442e09ac34940f05695197d7f5
Trictonicmp <code_shadow@hotmail.com>
2022-08-03 19:59:13 -0500

    Add to_json method to source class

--- lib/classes/source.rb
+++ lib/classes/source.rb
@@ -12,4 +12,12 @@ class Source
     @items << item unless @items.include?(item)
     item.source = self
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'name' => name,
+      'id' => id,
+    }.to_json(*args)
+  end
 end
