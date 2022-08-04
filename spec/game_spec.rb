3da193848848a237a5a96cb0bd14d4f47a052673
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-04 23:43:28 +0100

    Add unit tests for multiplayer game less than 10 years old

---
+++ spec/game_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/classes/game'
+
+describe Game do
+  context 'Recently Played Multi Player Game - Less than 10 Years Old' do
+    before :each do
+      @game_one = Game.new(true, '2021-11-03', '2018-05-25', false)
+    end
+
+    it 'Expects game to be instance of Game class' do
+      expect(@game_one).to be_instance_of Game
+    end
+
+    it 'Expects game to be a multiplayer game' do
+      expect(@game_one.multiplayer).to eq(true)
+    end
+
+    it 'Expects game not to be archived' do
+      expect(@game_one.archived).to eq(false)
+    end
+
+    it 'Expects game played within 2 years and less than 10 year old not to be archivable' do
+      archivable = @game_one.send(:can_be_archived?)
+      expect(archivable).to eq(false)
+    end
+  end
+end
