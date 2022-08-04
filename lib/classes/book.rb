ebaa1770c82e1bcb74fd94c59502adde0a6f5590
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-04 10:53:18 -0500

    Update Book class

--- lib/classes/book.rb
+++ lib/classes/book.rb
@@ -13,13 +13,18 @@ class Book < Item
     super || cover_state == 'bad'
   end
 
-  def to_json(*_args)
+  def to_json(*args)
     {
       id: id,
       publisher: @publisher,
       cover_state: @cover_state,
       publish_date: publish_date,
       archived: archived
+      label_id:@label.id,
+      source:@source.id,
+      author:@author.id,
+      genre:@genre.id,
+      JSON.create_id => self.class.name
     }.to_json
   end
 end
